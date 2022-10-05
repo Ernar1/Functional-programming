74127dfa6be0ff9d1e7de2e4604111fc6846e45c
Ernar1 <ernarmazhenov2000@gmail.com>
2022-10-05 14:08:09 +0600

    lab4.py

---
+++ lab4.py
@@ -0,0 +1,71 @@
+#1 esep
+""""
+string = str(input())
+upper = 0
+lower = 0
+for i in string:
+    if i.isupper():
+        upper += upper
+    else:
+        lower += lower
+if upper > lower:
+    print(string.upper())
+else:
+    print(string.lower())
+"""
+#2 esep
+""""
+a, b = input(), input()
+while not(a.isdigit() and b.isdigit()):
+    a, b = input(), input()
+print(int(a) + int(b))
+"""
+#3 esep
+""""
+s = 'python'
+print(s[:4])
+"""
+#4 esep
+""""
+n = 20
+m = 25
+prod = n * m
+print(f'Произведение {n} на {m} равно {prod}')
+"""
+#5 esep
+""""
+s = 'follow us @PYTHON'
+print(s.capitalize())
+"""
+#6 esep
+""""
+s = input()
+print(s.split('e'))
+"""
+#7 esep
+""""
+Str= "String"
+for index in range ( len ( Str ) ):
+    print ( Str[index])
+"""
+#8 esep
+""""
+Str= "String"
+for i,j in enumerate ( Str ):
+    print ( i,j )
+"""
+#9 esep
+""""
+Str= "String"
+print(Str.replace("i","a"))
+"""
+#10 esep
+""""
+str = input()
+print(str.lstrip())
+"""
+
+
+
+
+
